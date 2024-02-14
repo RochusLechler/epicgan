71c8b8634759ee02d00a7b4397ac68a100847d7b
RochusLechler <rochus.lechler@gmail.com>
2024-02-14 23:25:19 +0100

    minor fix on output folder

--- exec_kde.py
+++ exec_kde.py
@@ -1,4 +1,5 @@
-"""calculates KDE of number of particles in the jets for a given dataset
+"""Calculates KDE of number of particles in the jets for a given dataset.
+Name of dataset must be given when calling from command line.
 """
 
 
@@ -6,12 +7,15 @@
 
 if __name__ == "__main__":
 
+    import os
+    import sys
+
     from epicgan import data_proc
     from epicgan.utils import calc_kde
     import argparse
-    import os
 
-    folder = "/home/rochus/Documents/Studium/semester_pisa/cmepda/exam_project/JetNet_datasets"
+
+    folder = "./JetNet_datasets"
 
     parser = argparse.ArgumentParser(description="calculates KDE")
     parser.add_argument("dataset_name", help = "specify dataset for which to compute KDE")
@@ -19,6 +23,11 @@ if __name__ == "__main__":
 
     save_path = os.path.join(folder, str(args.dataset_name) + ".pkl")
 
-    dataset   = data_proc.get_dataset(str(args.dataset_name))
+    try:
+        dataset   = data_proc.get_dataset(str(args.dataset_name))
+    except FileNotFoundError:
+        print("that file does not exist, program will exit without doing anything")
+        sys.exit()
+
 
     kde = calc_kde(dataset, file_path = save_path)
